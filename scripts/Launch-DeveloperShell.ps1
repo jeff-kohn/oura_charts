e6bc1f7d7b2766415541c9df4fa6cb579831c77c
Jeff Kohn <jsk@kohn-family.com>
2024-06-12 21:05:03 +0000

    build stuff

--- scripts/Launch-DeveloperShell.ps1
+++ scripts/Launch-DeveloperShell.ps1
@@ -6,7 +6,7 @@
    This script will create the necessary build environment for the project so that the
    needed environment variables for using MSVC are set.
 
-  .PARAMETER TargetArch
+  .PARAMETER Arch
    Specifies the target architecture you're building for (x86 or amd64)
 
   .PARAMETER HostArch
@@ -17,11 +17,6 @@
    Specifies the platform toolset version to use for building (14.2x, 14.3x, etc). Currently
    defaults to "14.29", which is the latest VS2019 compiler toolset at the time of writing.
 
-  .INPUTS
-   None.
-
-  .OUTPUTS
-   None.
 
   .EXAMPLE
    PS> scripts/Launch-DeveloperShell.ps1 -TargetArch x86
@@ -33,17 +28,17 @@
 
 
 param (
-   [string] $TargetArch = "amd64",
+   [string] $Arch = "amd64",
    [string] $HostArch = "amd64"
 )
 
 $ErrorActionPreference = 'Stop'
 
-$VS2022Cmds = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Professional\Common7\Tools\VsDevCmd.bat", `
-              "$Env:ProgramFiles\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat", `
-              "$Env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+$VS2022Cmds = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Professional\Common7\Tools\Launch-VsDevShell.ps1", `
+              "$Env:ProgramFiles\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1", `
+              "$Env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\Launch-VsDevShell.ps1"
 
-Write-Output "`nLoooking for Visual Studio installation for VsDeCmd.bat..."
+Write-Output "`nLoooking for Visual Studio installation for Launch-VsDevShell.ps1..."
 $found = $false
 foreach ($path in $VS2022Cmds)
 {
@@ -53,8 +48,8 @@ foreach ($path in $VS2022Cmds)
       Write-Output "Found [$path]"
 
       # run the script to set up the environment
-      $params = " -arch=$TargetArch -host_arch=$HostArch "
-      Invoke-BatchFile -Path $path -Parameters $params
+      $params = " -Arch $Arch =HostArg $HostArch "
+      & $path $params
 
       Break
    }
@@ -68,11 +63,4 @@ if (-not $found)
    Write-Warning "`nCould not find Visual Studio 2022. If you get build errors, you may need to manually launch VS Developer
                   Command Prompt powershell before running build. Make sure you run the developer shell for the correct target architecture"
 }
-elseif ($env:VcToolsVersion.Contains($PlatformToolset))
-{
-   Write-Host "Build Environment successfully set up. You can run your build commands now."
-}
-else
-{
-   Write-Host "Build Environment was not successfully configured. Make sure you have the PowerShell Community Extensions install. (Install-Module Pscx)"
-}
+
