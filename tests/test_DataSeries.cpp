8f7248d26aa9ad6ae33107a7248bc95401f93efb
Jeff Kohn <jsk@kohn-family.com>
2024-07-04 00:35:16 -0500

    a little cleanup

--- tests/test_DataSeries.cpp
+++ tests/test_DataSeries.cpp
@@ -31,7 +31,7 @@ namespace oura_charts::test
 
       auto sleep_sessions = getDataSeries<SleepSession>(provider, detail::SortedPropertyMap{});
 
-//      auto by_weekday = groupBy < WeekdayMap<SleepSession>>(SleepSessionSeries{ sleep_sessions }, SessionWeekday, SleepTypeFilter{});
+      auto by_weekday = group<SleepByWeekday>(SleepSessionSeries{ sleep_sessions }, SessionWeekday, long_sleep_filter);
 //      auto by_year_month = groupByYearMonth(SleepSessionSeries{ sleep_sessions }, SleepTypeFilter{});
    }
 } // namespace oura_charts::test
