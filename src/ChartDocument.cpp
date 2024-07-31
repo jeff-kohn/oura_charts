6f4943da34e3ebf603a99eb2b3eb0d7b6caa4750
Jeff Kohn <jsk@kohn-family.com>
2024-07-30 23:30:40 -0500

    laptop wip

--- src/ChartDocument.cpp
+++ src/ChartDocument.cpp
@@ -3,21 +3,6 @@
 namespace oura_charts
 {
 
-   void ChartDocument::OnDraw([[maybe_unused]]wxDC*  dc)
-   {
-
-   }
-
-
-   void ChartDocument::OnUpdate([[maybe_unused]] wxView* sender, [[maybe_unused]] wxObject* hint)
-   {
-   }
-
-
-   bool ChartDocument::OnClose([[maybe_unused]] bool deleteWindow)
-   {
-      return true;
-   }
 
 
 } // namespace oura_charts
--- src/ChartDocument.h
+++ src/ChartDocument.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "oura_charts/chrono_helpers.h"
 #include <wx/docview.h>
 
 
@@ -9,9 +9,13 @@ namespace oura_charts
    class ChartDocument : public wxDocument
    {
    public:
-      void OnDraw(wxDC* dc);
-      void OnUpdate(wxView* sender, wxObject* hint = nullptr);
-      bool OnClose(bool deleteWindow = true);
+      ChartDocument() = default;
+      ~ChartDocument() override = default;
+
+
+   private:
+      year_month_day m_from_date{};
+      year_month_day m_until_date{};
    };
 
 } // namespace oura_charts
